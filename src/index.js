7ccb56974f3b85fcd780cd1818b697e1c4f6dc18
Phillip Castillo <PhillipACastillo@gmail.com>
2020-09-18 08:46:30 -0700

    Implement ApprovalCard and wrap CommentDetail in new component

--- src/index.js
+++ src/index.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-import CommentDetail from './CommentDetail'
-
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
   return (
     <div className = "ui container comments">
-      <CommentDetail
-        avatar = {faker.image.avatar()}
-        author = "Sam"
-        timeAgo = "Today at 4:00PM"
-        text = "Great last blog post!"
-      />
-      <CommentDetail
-        avatar = {faker.image.avatar()}
-        author = "Alex"
-        timeAgo = "Today at 2:00AM"
-        text = "Really learned something here."
-      />
-      <CommentDetail
-        avatar = {faker.image.avatar()}
-        author = "Jane"
-        timeAgo = "Yesterday at 5:00PM"
-        text = "Never gonna let you down."
-      />
+      <ApprovalCard>
+        <CommentDetail
+          avatar = {faker.image.avatar()}
+          author = "Sam"
+          timeAgo = "Today at 4:00PM"
+          text = "Great last blog post!"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          avatar = {faker.image.avatar()}
+          author = "Alex"
+          timeAgo = "Today at 2:00AM"
+          text = "Really learned something here."
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          avatar = {faker.image.avatar()}
+          author = "Jane"
+          timeAgo = "Yesterday at 5:00PM"
+          text = "Never gonna let you down."
+        />
+      </ApprovalCard>
     </div>
   )
 };
