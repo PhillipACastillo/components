1e9c3921a64469720e51906e6fc924b2a9b9a068
Phillip Castillo <PhillipACastillo@gmail.com>
2020-09-18 08:21:10 -0700

    Implement props inside of CommentDetail

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail'
+
+
 
 const App = () => {
   return (
     <div className = "ui container comments">
-
+      <CommentDetail
+        avatar = {faker.image.avatar()}
+        author = "Sam"
+        timeAgo = "Today at 4:00PM"
+        text = "Great last blog post!"
+      />
+      <CommentDetail
+        avatar = {faker.image.avatar()}
+        author = "Alex"
+        timeAgo = "Today at 2:00AM"
+        text = "Really learned something here."
+      />
+      <CommentDetail
+        avatar = {faker.image.avatar()}
+        author = "Jane"
+        timeAgo = "Yesterday at 5:00PM"
+        text = "Never gonna let you down."
+      />
     </div>
   )
 };
